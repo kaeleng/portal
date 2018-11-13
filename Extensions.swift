b5f17e842e2a14e51f1fb5a5f41e1def5beb3a82
Kaelen Guthrie <kguthrie@us.ci.org>
2018-11-12 19:25:54 -0700

    commgralj

---
+++ Extensions.swift
@@ -0,0 +1,47 @@
+//
+//  Extensions.swift
+//  Portal
+//
+//  Created by Kaelen Guthrie on 11/12/18.
+//  Copyright © 2018 Kaelen Guthrie. All rights reserved.
+//
+
+import Foundation
+import SceneKit
+import ARKit
+
+var width: CGFloat = 0.02
+var height: CGFloat = 1
+var length: CGFloat = 1
+
+var doorLength: CGFloat = 0.3
+
+func createBox(isDoor: Bool) -> SCNNode{
+    let node = SCNNode()
+    
+    //add inner box
+    let firstBox = SCNBox(width: width, height: height, length: isDoor ? doorLength: length, chamferRadius: 0)
+    let firstBoxNode = SCNNode(geometry: firstBox)
+    firstBoxNode.renderingOrder = 200
+    
+    node.addChildNode(firstBoxNode)
+    
+    //masked box
+    let maskedBox = SCNBox(width: width, height: height, length: isDoor ? doorLength: length, chamferRadius: 0)
+    maskedBox.firstMaterial?.diffuse.contents = UIColor.white
+    maskedBox.firstMaterial?.transparency = 0.00001
+    
+    let maskedBoxNode = SCNNode(geometry: maskedBox)
+    maskedBoxNode.renderingOrder = 100
+    maskedBoxNode.position = SCNVector3.init(width,0,0)
+    
+    node.addChildNode(maskedBoxNode)
+    
+    return node
+}
+
+extension FloatingPoint {
+    var degreesToRadians : Self {
+        return self * .pi/180
+    }
+}
